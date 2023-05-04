3a6c9656a38daaf9e3e30aeff7db092acb324eaf
Luis <luisverg71@gmail.com>
2023-05-04 09:00:51 -0300

    Initialize project

---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,10 @@
+function portfolio() {
+  return (
+    <div>
+      <h1 className="bg-red-600">Portfolio</h1>
+      <p>This is the portfolio page</p>
+    </div>
+  );
+}
+
+export default portfolio;
