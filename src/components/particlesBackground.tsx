3efe68dc95ae361c9fe2c36fd10248c064e3d3f5
Luis <luisverg71@gmail.com>
2023-05-05 11:23:00 -0300

    Add particles background

---
+++ src/components/particlesBackground.tsx
@@ -0,0 +1,111 @@
+import { useCallback } from "react";
+import type { Container, Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particlesConfig from "./config/particles-config";
+
+export default function Background() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    console.log(engine);
+
+    // you can initialize the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0d47a1",
+          },
+        },
+        fps_limit: 120,
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onclick: { enable: true, mode: "push" },
+            onhover: {
+              enable: true,
+              mode: "attract",
+              parallax: { enable: false, force: 60, smooth: 10 },
+            },
+            resize: true,
+          },
+          modes: {
+            push: { quantity: 4 },
+            attract: { distance: 200, duration: 0.4, factor: 5 },
+          },
+        },
+        particles: {
+          color: { value: "#ffffff" },
+          line_linked: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.4,
+            width: 1,
+          },
+          move: {
+            attract: { enable: false, rotateX: 600, rotateY: 1200 },
+            bounce: false,
+            direction: "none",
+            enable: true,
+            out_mode: "out",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: { density: { enable: true, value_area: 800 }, value: 80 },
+          opacity: {
+            anim: { enable: false, opacity_min: 0.1, speed: 1, sync: false },
+            random: false,
+            value: 0.5,
+          },
+          shape: {
+            character: {
+              fill: false,
+              font: "Verdana",
+              style: "",
+              value: "*",
+              weight: "400",
+            },
+            image: {
+              height: 100,
+              replace_color: true,
+              src: "images/github.svg",
+              width: 100,
+            },
+            polygon: { nb_sides: 5 },
+            stroke: { color: "#000000", width: 0 },
+            type: "circle",
+          },
+          size: {
+            anim: { enable: false, size_min: 0.1, speed: 40, sync: false },
+            random: true,
+            value: 5,
+          },
+        },
+        polygon: {
+          draw: { enable: false, lineColor: "#ffffff", lineWidth: 0.5 },
+          move: { radius: 10 },
+          scale: 1,
+          type: "none",
+          url: "",
+        },
+        retina_detect: true,
+      }}
+    />
+  );
+}
